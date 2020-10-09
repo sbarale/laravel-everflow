12b9c71e6a7200fe811eb217675177e2382f884b
Matheus Pratta <matpratta@gmail.com>
2020-10-09 16:56:16 -0300

    Network/Reporting

---
+++ src/Api/EverflowNetworkReporting.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace CodeGreenCreative\Everflow\Api;
+
+use CodeGreenCreative\Everflow\EverflowApiBase;
+use CodeGreenCreative\Everflow\EverflowHttpClient;
+
+class EverflowNetworkReporting extends EverflowApiBase
+{
+    public function summary()
+    {
+        return EverflowHttpClient::get(EverflowHttpClient::route('networks/dashboard/summary'));
+    }
+
+    public function entity($options = [])
+    {
+        return EverflowHttpClient::post(EverflowHttpClient::route('networks/reporting/entity'), $options);
+    }
+
+    public function conversion($conversionId)
+    {
+        return EverflowHttpClient::get(EverflowHttpClient::route('networks/reporting/conversions/:conversionId', [
+            'conversionId' => $conversionId
+        ]));
+    }
+
+    public function conversions($options = [])
+    {
+        return EverflowHttpClient::post(EverflowHttpClient::route('networks/reporting/conversions'), $options);
+    }
+
+    public function postconversions($options = [])
+    {
+        return EverflowHttpClient::post(EverflowHttpClient::route('networks/reporting/postconversions'), $options);
+    }
+
+    public function click($transactionId)
+    {
+        return EverflowHttpClient::get(EverflowHttpClient::route('networks/reporting/clicks/:transactionId', [
+            'transactionId' => $transactionId
+        ]));
+    }
+
+    public function clicks()
+    {
+        return EverflowHttpClient::get(EverflowHttpClient::route('networks/reporting/clicks'));
+    }
+
+    public function clickstream($options = [])
+    {
+        return EverflowHttpClient::post(EverflowHttpClient::route('networks/reporting/clicks/stream'), $options);
+    }
+
+    public function transaction($transactionId)
+    {
+        return EverflowHttpClient::get(EverflowHttpClient::route('networks/reporting/transactions/:transactionId', [
+            'transactionId' => $transactionId
+        ]));
+    }
+
+    public function variance($options = [])
+    {
+        return EverflowHttpClient::post(EverflowHttpClient::route('networks/reporting/variance'), $options);
+    }
+}
