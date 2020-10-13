3c77dd9c620c503cc99b76f13546baba650f5527
Matheus Pratta <matpratta@gmail.com>
2020-10-13 16:32:27 -0300

    Support for `asAffiliate()`

--- src/Everflow.php
+++ src/Everflow.php
@@ -10,7 +10,28 @@ class Everflow
         EverflowHttpClient::authNextRequestAs($apiKey);
 
         // Allows for function chaining
-        return new static;
+        return new $this;
+    }
+
+    public function asAffiliate($affiliate, $affiliateUserId = null)
+    {
+        // If no Affiliate user ID is set, get the token for the first available one
+        if (is_null($affiliateUserId)) {
+            // Gets the affiliate with the current API token
+            $affiliateUsers = $this->network()->affiliates($affiliate)->users()->all();
+
+            // Sets the user ID to first found user
+            $affiliateToken = $affiliateUsers->users[0]->relationship->api->api_key;
+        } else {
+            // If the user is set, only gets the user and their token
+            $affiliateUser = $this->network()->affiliates($affiliate)->users($affiliateUserId)->get();
+
+            // Sets the user ID to first found user
+            $affiliateToken = $affiliateUser->relationship->api->api_key;
+        }
+
+        // Allows for function chaining
+        return new $this->as($affiliateToken);
     }
 
     public function network()
