a0464461be6f09e17c0f19a6903cfdea498bcfbd
Matheus Pratta <matpratta@gmail.com>
2020-10-20 16:02:34 -0300

    Affiliate notifications now uses the Affiliate user key for querying the API

--- src/Api/EverflowNetworkAffiliates.php
+++ src/Api/EverflowNetworkAffiliates.php
@@ -48,7 +48,7 @@ class EverflowNetworkAffiliates extends EverflowApiBase
     public function notifications($pageSize = 5)
     {
         // Fetch the Affiliate's users
-        $users = $this->users()->all()->users;
+        $users = $this->asAdmin()->users()->all()->users;
 
         // If no users are found, return an error
         if (empty($users)) {
@@ -58,6 +58,6 @@ class EverflowNetworkAffiliates extends EverflowApiBase
         // Gets the first user and passes it to the notifications API call
         $user = $users[0];
         
-        return $this->users($user->network_affiliate_user_id)->notifications($pageSize);
+        return $this->asAffiliate($this->id(), $user->network_affiliate_user_id)->users($user->network_affiliate_user_id)->notifications($pageSize);
     }
 }
