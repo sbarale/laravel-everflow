b32f8eaca3c487f7308a2b0c45cf0a9051b8093b
Matheus Pratta <matpratta@gmail.com>
2020-12-17 15:54:32 -0300

    Support for affiliate billing summary

--- src/Api/EverflowAffiliateBillings.php
+++ src/Api/EverflowAffiliateBillings.php
@@ -9,6 +9,23 @@ class EverflowAffiliateBillings extends EverflowApiBase
 {
     public function invoicestable($data = [])
     {
-        return EverflowHttpClient::post(EverflowHttpClient::route('affiliates/billings/affiliates/invoicestable'), $data);
+        $page = isset($data['page']) ? $data['page'] : 1;
+        $page_size = isset($data['page_size']) ? $data['page_size'] : config('everflow.per_page');
+
+        return EverflowHttpClient::post(EverflowHttpClient::route('affiliates/billings/affiliates/invoicestable', [], [
+            'page' => $page,
+            'page_size' => $page_size,
+        ]), $data);
+    }
+
+    public function summary($data = [])
+    {
+        $page = isset($data['page']) ? $data['page'] : 1;
+        $page_size = isset($data['page_size']) ? $data['page_size'] : config('everflow.per_page');
+
+        return EverflowHttpClient::post(EverflowHttpClient::route('affiliates/billings/affiliates/summary', [], [
+            'page' => $page,
+            'page_size' => $page_size,
+        ]), $data);
     }
 }
