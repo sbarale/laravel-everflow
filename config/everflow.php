483f1558acaa3006752f3fb0fc2bcdb633f400c7
Jesse Vista <jvista@upwebdesign.com>
2021-02-03 15:08:19 -0500

    add network_id

--- config/everflow.php
+++ config/everflow.php
@@ -13,10 +13,10 @@ return [
 
     // Outputs data to your laravel.log file for debugging
     'debug' => false,
-
     // Define the Everflow API key being used
     'api_key' => env('EVERFLOW_API_KEY'),
-
     // Define how many entities per page the API should be fetching by default
     'per_page' => 100,
+    // Your network ID from Everflow
+    'network_id' => env('EVERFLOW_NETWORK_ID')
 ];
