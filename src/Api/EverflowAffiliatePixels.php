ad41fcae46c85fe2d8f53e446de83f86e7b377bd
Sebastian Barale <sebasinar@gmail.com>
2021-05-04 17:21:40 -0300

    Fixed pixels

--- src/Api/EverflowAffiliatePixels.php
+++ src/Api/EverflowAffiliatePixels.php
@@ -9,24 +9,24 @@ class EverflowAffiliatePixels extends EverflowApiBase
 {
     public function all()
     {
-        return EverflowHttpClient::get(EverflowHttpClient::route('networks/pixels'));
+        return EverflowHttpClient::get(EverflowHttpClient::route('affiliates/pixels'));
     }
 
     public function get()
     {
-        return EverflowHttpClient::get(EverflowHttpClient::route('networks/pixels/:pixelId', [
+        return EverflowHttpClient::get(EverflowHttpClient::route('affiliates/pixels/:pixelId', [
             'pixelId' => $this->id(),
         ]));
     }
 
     public function create($data = [])
     {
-        return EverflowHttpClient::post(EverflowHttpClient::route('networks/pixels'), $data);
+        return EverflowHttpClient::post(EverflowHttpClient::route('affiliates/pixels'), $data);
     }
 
     public function update($data = [])
     {
-        return EverflowHttpClient::put(EverflowHttpClient::route('networks/pixels/:pixelId', [
+        return EverflowHttpClient::put(EverflowHttpClient::route('affiliates/pixels/:pixelId', [
             'pixelId' => $this->id(),
         ]), $data);
     }
